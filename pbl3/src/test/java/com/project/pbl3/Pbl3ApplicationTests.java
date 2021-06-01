d2d6aa7cdfaae6126daba6320d0ea7bd3ffed654
EinVaann <44147557+EinVaann@users.noreply.github.com>
2021-06-01 20:04:49 +0700

    Đây là bản sửa hoàn thiện. Chỉ xong hiển thị student. Chưa có teacher

---
+++ pbl3/src/test/java/com/project/pbl3/Pbl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pbl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbl3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
