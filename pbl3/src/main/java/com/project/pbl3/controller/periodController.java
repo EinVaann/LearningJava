ee202bb534496d3eefded4f64a35c9d0865f8ff3
EinVaann <Einacht3012@gmail.com>
2021-06-12 15:49:47 +0700

    Period

--- pbl3/src/main/java/com/project/pbl3/controller/periodController.java
+++ pbl3/src/main/java/com/project/pbl3/controller/periodController.java
@@ -1,12 +1,16 @@
 package com.project.pbl3.controller;
 
 import com.project.pbl3.model.periods;
+import com.project.pbl3.model.subjects;
 import com.project.pbl3.service.PeriodService;
-import org.apache.logging.log4j.message.Message;
+import com.project.pbl3.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,22 +18,25 @@ import java.util.List;
 public class periodController {
     @Autowired
     private PeriodService periodService;
+    @Autowired
+    private SubjectService subjectService;
 
     @GetMapping("/period")
     private String GetPeriod(Model model){
-        List<periods> ListPeriods = periodService.findAll();
-//        for (periods period: ListPeriods
-//             ) {
-//            period.setPeriod(3);
-//            periodService.save(period);
-//        }
-        model.addAttribute("periods",ListPeriods);
-
+        List<periods> periodsList = periodService.findAll();
+        List<subjects> subjectsList = subjectService.findAll();
+        //System.out.print(periodsList.size());
+        model.addAttribute("periodList",periodsList);
+        model.addAttribute("subjectList",subjectsList);
         return "Period";
     }
-//    @RequestMapping(value = "/save/", method = RequestMethod.POST)
-//    public String controllerMethod(@RequestParam(value="myArray[]") List<String> Array){
-//        System.out.println("My Array"+Array.get(0));
-//        return "class";
-//    }
+
+    @PostMapping("/edit-period")
+    private String save(@ModelAttribute("periods") periods periods) throws Exception {
+        periods newPeriod = periodService.findByID(periods.getID()+1);
+        newPeriod.setPeriod(periods.getPeriod());
+        periodService.save(newPeriod);
+        return "redirect:/period";
+    }
+
 }
